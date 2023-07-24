c63566c920abf6f0f52f975d25443ebb1e0e9ec4
Rafael Naoki Arakaki Uyeta <rafa.naoki@gmail.com>
2023-07-24 15:18:59 -0300

    att

--- LocacaoBike/src/main/java/br/ufscar/dc/dsw/LocadoraMvcApplication.java
+++ LocacaoBike/src/main/java/br/ufscar/dc/dsw/LocadoraMvcApplication.java
@@ -1,5 +1,7 @@
 package br.ufscar.dc.dsw;
 
+import java.util.List;
+
 import java.math.BigDecimal;
 
 import org.springframework.boot.CommandLineRunner;
@@ -34,6 +36,23 @@ public class LocadoraMvcApplication {
             l1.setCNPJ("55.789.390/0008-99");
             l1.setCidade("Ribeirão Preto");
             l1.setTelefone("(16)12345-1234");
+            locadoraDAO.save(l1);
+
+            Locadora l2 = new Locadora();
+            l2.setNome("Conserta Bike");
+            l2.setEmail("conserta_bikeRP@gmail.com");
+            l2.setSenha("ConsertaBike123");
+            l2.setPapel("Locadora");
+            l2.setCNPJ("71.150.470/0001-40");
+            l2.setCidade("São Carlos");
+            l2.setTelefone("(16)12345-1234");
+            locadoraDAO.save(l2);
+
+            List<Locadora> locadora = locadoraDAO.findAll();
+            System.out.println("Printando todas as locadoras adicionadas");
+            for(Locadora l : locadora) {
+                  System.out.println(l);
+            }
 
 		};
 	}
