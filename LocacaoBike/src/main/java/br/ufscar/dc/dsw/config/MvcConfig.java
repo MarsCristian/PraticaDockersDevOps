4e86186c62930c713e32c32b72f71569fae5b301
Portix-S <vitorsilva@estudante.ufscar.br>
2023-07-30 23:34:32 -0300

    Começo Login

--- LocacaoBike/src/main/java/br/ufscar/dc/dsw/config/MvcConfig.java
+++ LocacaoBike/src/main/java/br/ufscar/dc/dsw/config/MvcConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -16,11 +17,11 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 @ComponentScan(basePackages = "br.ufscar.dc.dsw.config")
 public class MvcConfig implements WebMvcConfigurer {
 
-    // public void addViewControllers(ViewControllerRegistry registry) {
-	// 	registry.addViewController("/").setViewName("redirect:/home");
-	// 	registry.addViewController("/home").setViewName("home");
-	// 	registry.addViewController("/login").setViewName("login");
-	// }
+    public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("redirect:/home");
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/login").setViewName("login");
+	}
 
     @Bean
     public LocaleResolver localeResolver() {
--- LocacaoBike/src/main/java/br/ufscar/dc/dsw/dao/IUsuarioDAO.java
+++ LocacaoBike/src/main/java/br/ufscar/dc/dsw/dao/IUsuarioDAO.java
@@ -2,7 +2,9 @@ package br.ufscar.dc.dsw.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import br.ufscar.dc.dsw.domain.Usuario;
 
@@ -17,6 +19,6 @@ public interface IUsuarioDAO extends CrudRepository<Usuario, Long>{
 
 	void deleteById(Long id);
 
-	// @Query("SELECT u FROM Usuario u WHERE u.username = :username")
-    // public Usuario getUserByUsername(@Param("username") String username);
+	@Query("SELECT u FROM Usuario u WHERE u.email = :email")
+    public Usuario getUserByUsername(@Param("email") String email);
 }
