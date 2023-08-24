97f4c51b0251c0f714e220812c6c9b6dfe8539d3
Notargg <112586297+Notargg@users.noreply.github.com>
2023-08-24 10:42:37 -0300

    Update LocacaoRestController.java
    Terminar

--- LocacaoBike/src/main/java/br/ufscar/dc/dsw/controller/LocacaoRestController.java
+++ LocacaoBike/src/main/java/br/ufscar/dc/dsw/controller/LocacaoRestController.java
@@ -61,9 +61,6 @@ public class LocacaoRestController {
 	@Autowired
 	private IClienteService clienteService;
 
-	@Autowired
-	private IUsuarioService usuarioService;
-
 
     private boolean isJSONValid(String jsonInString) {
 		try {
@@ -72,6 +69,30 @@ public class LocacaoRestController {
 			return false;
 		}
 	}
+
+	@GetMapping(path = "/locacoes")
+	public ResponseEntity<List<Locacao>> lista() {
+		List<Locacao> lista = locacaoService.buscarTodos();
+		System.out.println("Entrou no lista");
+
+		if (lista.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(lista);
+	}
+
+	@GetMapping(path = "/locacoes/{id}")
+	public ResponseEntity<Locacao> listaLocacao(@PathVariable("id") Long id) {
+		Locacao locacao = locacaoService.buscarPorId(id);
+		System.out.println("Entrou no listaLocacao");
+
+		if(locacao == null) {
+			return ResponseEntity.notFound().build();
+		}
+
+		return ResponseEntity.ok(locacao);
+	}
+
 	
 	@GetMapping(path = "/locacoes/clientes/{id}")
 	public ResponseEntity<List<Locacao>> listaLocacoesCliente(@PathVariable("id") Long id) {
@@ -86,119 +107,19 @@ public class LocacaoRestController {
 		return ResponseEntity.ok(locacoes);
 	}
 
-	@GetMapping("/cadastrar")
-	public String cadastrar(Locacao locacao, ModelMap model, @AuthenticationPrincipal Usuario usuario) {
-		System.out.println("Entrou no cadastrar");
-		DataAtual = formatoData.format(new Date());
-		System.out.println(DataAtual);
-		HoraAtual = formatoHora.format(new Date());
-		System.out.println(HoraAtual);
-		DataHoraAtual = DataAtual + "T" + HoraAtual + ":00";
-		System.out.println(DataHoraAtual);
-		model.addAttribute("dataHora", DataHoraAtual);
-		model.addAttribute("usuarioTeste", usuario);
+	@GetMapping(path = "/locacoes/locadoras/{id}")
+	public ResponseEntity<List<Locacao>> listaLocacoesLocadora(@PathVariable("id") Long id) {
+		Locadora locadora = locadoraService.buscarPorId(id);
+		List<Locacao> locacoes = locacaoService.buscarTodosPorLocadora(locadora);
+		System.out.println("Entrou no listaLocacoesLocadora");
 
-		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		String nome = null;
-		if (principal instanceof UserDetails) {
-			nome = ((UserDetails)principal).getUsername();
+		if(locacoes.isEmpty()) {
+			return ResponseEntity.notFound().build();
 		}
 
-		Usuario teste = usuarioService.buscarPorNome(nome);
-
-		model.addAttribute("idCliente", teste.getId());
-		return "locacao/cadastro";
+		return ResponseEntity.ok(locacoes);
 	}
 
-	@GetMapping("/listar")
-	public String listar(ModelMap model) {
-		System.out.println("Entrou no listar");
-		String dataHoraAuxiliar = null;
-		List<Locacao> locacoes = locacaoService.buscarTodos();
-		for (Locacao locacao : locacoes) {
-			dataHoraAuxiliar = locacao.getDataHora().replace("T", " ");
-			locacao.setDataHora(dataHoraAuxiliar);
-		}
-
-		model.addAttribute("locacoes", locacoes);
-
-		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		String nome = null;
-		if (principal instanceof UserDetails) {
-			nome = ((UserDetails)principal).getUsername();
-		}
-
-		System.out.println("achou o usuario: " + nome);
-		Usuario teste = usuarioService.buscarPorNome(nome);
-		model.addAttribute("cliente", teste);
-
-		return "locacao/lista";
-	}
-
-	@PostMapping("/salvar")
-	public String salvar(@Valid Locacao locacao, BindingResult result, RedirectAttributes attr) {
-		System.out.println("Entrou no salvar");
-
-		System.out.println("Erros: " + result.getErrorCount());
-
-		if (result.hasErrors()) {
-			System.out.println("Entrou no if do salvar locacao");
-			return "locacao/cadastro";
-		}
-		locacaoService.salvar(locacao);
-		attr.addFlashAttribute("sucess", "Locação inserida com sucesso");
-		return "redirect:/locacoes/listar";
-	}
-
-	@GetMapping("/editar/{id}")
-	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
-		System.out.println("Entrou no preEditar");
-		model.addAttribute("locacao", locacaoService.buscarPorId(id));		
-		return "locacao/cadastro";
-	}
-
-	@PostMapping("/editar")
-	public String editar(@Valid Locacao locacao, BindingResult result, RedirectAttributes attr) {
-		System.out.println("Entrou no editar");
-
-		Integer errors = 0;
-		if (result.getFieldError("dataHora") != null)
-			errors += 1;
-		System.out.println(errors);
-		System.out.println(result.getFieldErrorCount()); 
-		if (result.getFieldErrorCount() > errors+1 || result.getFieldError("dataHora") != null) {
-			System.out.println("Falhou");
-
-			return "locacao/cadastro";
-		}
-
-		locacaoService.salvar(locacao);
-		attr.addFlashAttribute("sucess", "Locação editada com sucesso.");
-		return "redirect:/locacoes/listar";
-	}
-
-	@GetMapping("/excluirPorId/{id}") 
-	public String excluirPorId(@PathVariable("id") Long id, RedirectAttributes attr) {
-		System.out.println("Entrou no excluirPorId");
-		locacaoService.excluirPorId(id);
-		attr.addFlashAttribute("sucess", "Locação excluída com sucesso.");
-		return "redirect:/locacoes/listar";
-	}
-
-	@ModelAttribute("clientes")
-	public List<Cliente> listaClientes() {
-		return clienteService.buscarTodos();
-	}
-
-	@ModelAttribute("clienteAtual")
-	public Cliente listaClienteAtual(@AuthenticationPrincipal Cliente usuario) {
-		System.out.println("usuario" + usuario);
-		return usuario;
-	}
-
-	@ModelAttribute("locadoras")
-	public List<Locadora> listaLocadoras() {
-		return locadoraService.buscarTodos();
-	}
+	
 }
 
